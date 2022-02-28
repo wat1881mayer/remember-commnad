6bf3c8f5e44381d305f2c7d2d2a8634594aad9c2
wat1881mayer <happymayer2013@icloud.com>
2022-02-28 22:49:45 +0900

    confirm baseURL

--- client/api/build-client.ts
+++ client/api/build-client.ts
@@ -4,13 +4,12 @@ import { NextPageContext } from 'next';
 const buildClient = ({ req }: any) => {
   if (typeof window === 'undefined') {
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://www.remember-cli.com/',
       headers: req.headers,
     });
   } else {
     return axios.create({
-      baseURL: 'http://www.remember-cli.com/',
+      baseURL: '/',
     });
   }
 };
