c21c6ef9dee3b27b61b30fc59e69ea3bee90102c
wat1881mayer <happymayer2013@icloud.com>
2022-03-04 14:05:32 +0900

    specific domain

--- client/pages/testlist.tsx
+++ client/pages/testlist.tsx
@@ -108,9 +108,12 @@ const Test: NextPage<Props> = ({ tests }) => {
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const headers: any = context.req.headers;
-  const { data } = await axios.get(`${domain.kubernetes}api/questions`, {
-    headers: headers,
-  });
+  const { data } = await axios.get(
+    'http://www.rememeber-cli.com/api/questions',
+    {
+      headers: headers,
+    }
+  );
   return {
     props: { tests: data },
   };
--- client/src/config/prod.ts
+++ client/src/config/prod.ts
@@ -1,5 +1,4 @@
 export const prodDomain = {
   host: 'http://www.rememeber-cli.com/',
-  kubernetes:
-    'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/',
+  kubernetes: 'http://www.rememeber-cli.com/',
 };
