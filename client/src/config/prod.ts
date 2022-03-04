b3de3b88a3bc9dd2a07c5670a9efadb3cd6a4860
wat1881mayer <happymayer2013@icloud.com>
2022-03-04 13:53:02 +0900

    fix domain kubernetes

--- client/src/config/prod.ts
+++ client/src/config/prod.ts
@@ -1,4 +1,5 @@
 export const prodDomain = {
   host: 'http://www.rememeber-cli.com/',
-  kubernetes: 'http://www.rememeber-cli.com/',
+  kubernetes:
+    'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/',
 };
